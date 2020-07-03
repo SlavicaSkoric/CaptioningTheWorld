16ee2185aad04b2b7f749de8997c22e3d89b03de
Slavica Skorić <64714350+SlavicaSkoric@users.noreply.github.com>
2020-07-04 01:13:05 +0200

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,29 @@
+function upDate1(previewPic){
+    document.getElementById("photo1index1").style.filter = 'grayscale(0)';    
+    document.getElementById("captionMe1").style.visibility = 'visible';
+}
+
+function unDo1(){
+    document.getElementById("photo1index1").style.filter = 'grayscale(1)';    
+    document.getElementById("captionMe1").style.visibility = 'hidden';    
+}
+
+function upDate2(previewPic){
+    document.getElementById("photo2index1").style.filter = 'grayscale(0)';
+    document.getElementById("captionMe2").style.visibility = 'visible';
+}
+
+function unDo2(){
+    document.getElementById("photo2index1").style.filter = 'grayscale(1)';
+    document.getElementById("captionMe2").style.visibility = 'hidden';  
+}
+
+function upDate3(previewPic){
+    document.getElementById("photo3index1").style.filter = 'grayscale(0)';
+    document.getElementById("captionMe3").style.visibility = 'visible';   
+}
+
+function unDo3(){
+    document.getElementById("photo3index1").style.filter = 'grayscale(1)';
+    document.getElementById("captionMe3").style.visibility = 'hidden'; 
+}
